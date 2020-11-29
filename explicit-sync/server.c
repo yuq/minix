e803efa2a5bfc7a56f199b7acb3b50231496e4a6
Qiang Yu <yuq825@gmail.com>
2020-11-29 15:49:44 +0800

    add explicit-sync

---
+++ explicit-sync/server.c
@@ -0,0 +1,363 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/epoll.h>
+
+#include <xf86drm.h>
+#include <xf86drmMode.h>
+
+#include "share.h"
+
+static struct render_state state;
+
+static int drm_fd;
+static drmModeConnectorPtr connector = NULL;
+static drmModeFBPtr orig_fb;
+static drmModeCrtcPtr crtc;
+
+static void display_init(void)
+{
+	int fd = open("/dev/dri/card0", O_RDWR);
+	assert(fd >= 0);
+
+	drmModeResPtr res = drmModeGetResources(fd);
+	assert(res);
+
+	for (int i = 0; i < res->count_connectors; i++) {
+		connector = drmModeGetConnector(fd, res->connectors[i]);
+		assert(connector);
+
+		// find a connected connection
+		if (connector->connection == DRM_MODE_CONNECTED)
+			break;
+
+		drmFree(connector);
+		connector = NULL;
+	}
+	assert(connector);
+
+	drmModeEncoderPtr encoder = drmModeGetEncoder(fd, connector->encoder_id);
+	assert(encoder);
+
+	crtc = drmModeGetCrtc(fd, encoder->crtc_id);
+	assert(crtc);
+
+	// original fb used for terminal
+	orig_fb = drmModeGetFB(fd, crtc->buffer_id);
+	assert(orig_fb);
+
+	drm_fd = fd;
+	state.fd = fd;
+	state.target_width = orig_fb->width;
+	state.target_height = orig_fb->height;
+
+	drmFree(encoder);
+	drmFree(res);
+}
+
+static const char vertex_shader[] =
+	"attribute vec3 positionIn;\n"
+	"attribute vec2 texcoordIn;\n"
+	"varying vec2 texcoord;\n"
+	"void main()\n"
+	"{\n"
+	"    gl_Position = vec4(positionIn, 1);\n"
+	"    texcoord = texcoordIn;\n"
+	"}\n";
+
+static const char fragment_shader[] =
+	"precision mediump float;\n"
+	"uniform sampler2D texMap;\n"
+	"varying vec2 texcoord;\n"
+	"void main() {\n"
+	"    gl_FragColor = texture2D(texMap, texcoord);\n"
+	"}\n";
+
+static void composite(int fd)
+{
+	struct present_buffer data;
+	int fds[2];
+	int num_fd = 2;
+
+	// get new window frame from client
+	ssize_t size = sock_fd_read(fd, &data, sizeof(data), fds, &num_fd);
+	assert(size > 0);
+	assert(num_fd >= 1);
+
+	int buffer_fd = fds[0];
+	int wait_fd = num_fd == 2 ? fds[1] : -1;
+
+	struct gbm_import_fd_data gbm_data = {
+		.fd = buffer_fd,
+		.width = data.width,
+		.height = data.height,
+		.stride = data.stride,
+		.format = data.format,
+	};
+
+	struct gbm_bo *bo = gbm_bo_import(
+		state.gbm, GBM_BO_IMPORT_FD,
+		&gbm_data, GBM_BO_USE_RENDERING);
+	assert(bo);
+
+	// close after usage
+	close(buffer_fd);
+
+	epoxy_has_egl_extension(state.display, "EGL_KHR_image_pixmap");
+
+	EGLImageKHR image = eglCreateImageKHR(
+		state.display, state.context,
+		EGL_NATIVE_PIXMAP_KHR, bo, NULL);
+	assert(image != EGL_NO_IMAGE_KHR);
+
+	// destroy after usage
+	gbm_bo_destroy(bo);
+
+	glActiveTexture(GL_TEXTURE0);
+
+	epoxy_has_gl_extension("GL_OES_EGL_image");
+
+	GLuint texid;
+	glGenTextures(1, &texid);
+	glBindTexture(GL_TEXTURE_2D, texid);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glEGLImageTargetTexture2DOES(GL_TEXTURE_2D, image);
+
+	// destroy after usage
+	eglDestroyImageKHR(state.display, image);
+
+	// wait client render is done before composite
+	if (wait_fd >= 0) {
+		wait_fence(state.display, wait_fd);
+		close(wait_fd);
+	}
+
+	GLfloat x = -1.0 + data.x * 2.0 / state.target_width;
+	GLfloat y = 1.0 - data.y * 2.0 / state.target_height;
+	GLfloat w = x + data.width * 2.0 / state.target_width;
+	GLfloat h = y - data.height * 2.0 / state.target_height;
+		
+	GLfloat vertex[] = {
+		x, h, 0,
+		x, y, 0,
+		w, y, 0,
+		w, h, 0,
+	};
+
+	GLfloat texcoord[] = {
+		0, 1,
+		0, 0,
+		1, 0,
+		1, 1,
+	};
+
+	GLushort index[] = {
+		0, 1, 3,
+		1, 2, 3,
+	};
+
+	GLint pos = glGetAttribLocation(state.program, "positionIn");
+        glEnableVertexAttribArray(pos);
+	glVertexAttribPointer(pos, 3, GL_FLOAT, 0, 0, vertex);
+
+	GLint tex = glGetAttribLocation(state.program, "texcoordIn");
+        glEnableVertexAttribArray(tex);
+	glVertexAttribPointer(tex, 2, GL_FLOAT, 0, 0, texcoord);
+
+	GLint texMap = glGetUniformLocation(state.program, "texMap");
+	glUniform1i(texMap, 0); // GL_TEXTURE0
+
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glDrawElements(GL_TRIANGLES, sizeof(index)/sizeof(GLushort), GL_UNSIGNED_SHORT, index);
+
+	// after composite is done, this fence will be signaled
+	int signal_fd = get_fence(state.display);
+
+	// swap back buffer to front
+	eglSwapBuffers(state.display, state.surface);
+
+	// delete after usage
+	glDeleteTextures(1, &texid);
+
+	// infom client window frame has been consumed
+	struct present_done done = {
+		.index = data.index,
+	};
+	if (signal_fd >= 0) {
+		size = sock_fd_write(fd, &done, sizeof(done), &signal_fd, 1);
+		close(signal_fd);
+	} else
+		size = sock_fd_write(fd, &done, sizeof(done), NULL, 0);
+	assert(size > 0);
+}
+
+struct display_framebuffer {
+	struct gbm_bo *bo;
+	uint32_t fb_id;
+	struct display_framebuffer *next;
+};
+
+// assume max number of bos in a gbm_surface is less than 32
+#define MAX_BOS 32
+struct display_framebuffer fbs[MAX_BOS] = {0};
+// fb which is on screen
+struct display_framebuffer *showing_fb = NULL;
+// fbs pending to be show on screen
+struct display_framebuffer *pending_fbs = NULL;
+
+static void display_output(void)
+{
+	struct gbm_bo *bo = gbm_surface_lock_front_buffer(state.gs);
+	assert(bo);
+
+	// find existing framebuffer, create one if not exist
+	struct display_framebuffer *fb = NULL;
+	for (int i = 0; i < MAX_BOS; i++) {
+		if (!fbs[i].bo || fbs[i].bo == bo) {
+			fb = fbs + i;
+			break;
+		}
+	}
+
+	if (!fb->bo) {
+		assert(!drmModeAddFB(drm_fd, gbm_bo_get_width(bo),
+				     gbm_bo_get_height(bo), 24,
+				     gbm_bo_get_bpp(bo),
+				     gbm_bo_get_stride(bo),
+				     gbm_bo_get_handle(bo).u32,
+				     &fb->fb_id));
+		fb->bo = bo;
+	}
+
+	fb->next = NULL;
+	if (!pending_fbs) {
+		// need to kick start page flip first time
+		assert(!drmModePageFlip(drm_fd, crtc->crtc_id, fb->fb_id,
+					DRM_MODE_PAGE_FLIP_EVENT, NULL));
+		pending_fbs = fb;
+	} else {
+		// pend page flip request will be consumed by drm event handler
+		struct display_framebuffer *pfb = pending_fbs;
+		// queue request to list tail
+		while (pfb->next) pfb = pfb->next;
+		pfb->next = fb;
+	}
+}
+
+static void
+page_flip_handler(int fd, uint32_t frame, uint32_t sec, uint32_t usec,
+		  void *user_ptr)
+{
+	assert(pending_fbs);
+
+	// release replaced previous showing framebuffer
+	if (showing_fb)
+		gbm_surface_release_buffer(state.gs, showing_fb->bo);
+
+	showing_fb = pending_fbs;
+	pending_fbs = pending_fbs->next;
+	if (pending_fbs)
+		assert(!drmModePageFlip(drm_fd, crtc->crtc_id, pending_fbs->fb_id,
+					DRM_MODE_PAGE_FLIP_EVENT, NULL));
+}
+
+static int dispatch_fd;
+
+static void dispatch_add(int fd)
+{
+	struct epoll_event event = {
+		.events = EPOLLIN,
+		.data.fd = fd,
+	};
+	assert(!epoll_ctl(dispatch_fd, EPOLL_CTL_ADD, fd, &event));
+}
+
+static void dispatch_remove(int fd)
+{
+	assert(!epoll_ctl(dispatch_fd, EPOLL_CTL_DEL, fd, NULL));
+}
+
+static bool stop = false;
+
+static void sigint_handler(int arg)
+{
+	stop = true;
+}
+
+void server_main(int fd)
+{
+	// register CTRL+C terminate interrupt
+	signal(SIGINT, sigint_handler);
+
+	// init display
+	display_init();
+
+	// init render
+	render_target_init(&state);
+	init_gles(&state, vertex_shader, fragment_shader);
+
+	// background color
+	glClearColor(0.15, 0.15, 0.15, 0);
+
+	dispatch_fd = epoll_create1(0);
+	assert(dispatch_fd >= 0);
+
+	dispatch_add(drm_fd);
+	dispatch_add(fd);
+
+	bool client_added = true;
+	while (!stop) {
+		struct epoll_event events[64];
+
+		int n = epoll_wait(dispatch_fd, events, 64, -1);
+		if (n < 0)
+			break;
+
+		for (int i = 0; i < n; i++) {
+			int efd = events[i].data.fd;
+
+			assert(events[i].events == EPOLLIN);
+
+			if (efd == drm_fd) {
+				drmEventContext ev = {
+					.version = DRM_EVENT_CONTEXT_VERSION,
+					.page_flip_handler = page_flip_handler,
+				};
+				assert(!drmHandleEvent(efd, &ev));
+			} else if (efd == fd) {
+				// get client output and past on fb
+				composite(fd);
+
+				// show on screen
+				display_output();
+			} else {
+				fprintf(stderr, "invalid epoll event fd %d\n", efd);
+				exit(1);
+			}
+		}
+
+		if (gbm_surface_has_free_buffers(state.gs)) {
+			if (!client_added) {
+				dispatch_add(fd);
+				client_added = true;
+			}
+		} else {
+			// does not handle new client request if no free framebuffer
+			if (client_added) {
+				dispatch_remove(fd);
+				client_added = false;
+			}
+		}
+	}
+
+	// restore previous fb
+	assert(!drmModeSetCrtc(drm_fd, crtc->crtc_id, orig_fb->fb_id, 0, 0,
+			       &connector->connector_id, 1, &crtc->mode));
+}
---
+++ explicit-sync/share.c
@@ -0,0 +1,295 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/socket.h>
+
+#include "share.h"
+
+ssize_t
+sock_fd_write(int sock, void *buf, ssize_t buflen, int *fds, int num_fd)
+{
+    ssize_t size;
+    struct msghdr msg;
+    struct iovec iov;
+    union {
+        struct cmsghdr  cmsghdr;
+        char control[CMSG_SPACE(sizeof (int) * num_fd)];
+    } cmsgu;
+    struct cmsghdr  *cmsg;
+
+    iov.iov_base = buf;
+    iov.iov_len = buflen;
+
+    msg.msg_name = NULL;
+    msg.msg_namelen = 0;
+    msg.msg_iov = &iov;
+    msg.msg_iovlen = 1;
+
+    if (fds) {
+        msg.msg_control = cmsgu.control;
+        msg.msg_controllen = sizeof(cmsgu.control);
+
+        cmsg = CMSG_FIRSTHDR(&msg);
+        cmsg->cmsg_len = CMSG_LEN(sizeof (int) * num_fd);
+        cmsg->cmsg_level = SOL_SOCKET;
+        cmsg->cmsg_type = SCM_RIGHTS;
+
+	memcpy(CMSG_DATA(cmsg), fds, sizeof (int) * num_fd);
+    } else {
+        msg.msg_control = NULL;
+        msg.msg_controllen = 0;
+    }
+
+    size = sendmsg(sock, &msg, 0);
+
+    if (size < 0)
+        perror ("sendmsg");
+    return size;
+}
+
+ssize_t
+sock_fd_read(int sock, void *buf, ssize_t bufsize, int *fds, int *num_fd)
+{
+    ssize_t size;
+
+    if (fds) {
+        struct msghdr msg;
+        struct iovec iov;
+	int max_fds = *num_fd;
+        union {
+            struct cmsghdr cmsghdr;
+            char control[CMSG_SPACE(sizeof (int) * max_fds)];
+        } cmsgu;
+
+        iov.iov_base = buf;
+        iov.iov_len = bufsize;
+
+        msg.msg_name = NULL;
+        msg.msg_namelen = 0;
+        msg.msg_iov = &iov;
+        msg.msg_iovlen = 1;
+        msg.msg_control = cmsgu.control;
+        msg.msg_controllen = sizeof(cmsgu.control);
+        size = recvmsg (sock, &msg, 0);
+        if (size < 0) {
+            perror ("recvmsg");
+            exit(1);
+        }
+
+	int offset = 0;
+	struct cmsghdr *hdr;
+	if (msg.msg_controllen >= sizeof(struct cmsghdr)) {
+		for (hdr = CMSG_FIRSTHDR(&msg); hdr; hdr = CMSG_NXTHDR(&msg, hdr)) {
+			if (hdr->cmsg_level == SOL_SOCKET && hdr->cmsg_type == SCM_RIGHTS) {
+				int nfd = (hdr->cmsg_len - CMSG_LEN(0)) / sizeof (int);
+				assert(nfd <= max_fds - offset);
+				memcpy(fds + offset, CMSG_DATA(hdr), nfd * sizeof (int));
+				offset += nfd;
+			}
+		}
+	}
+	*num_fd = offset;
+    } else {
+        size = read (sock, buf, bufsize);
+        if (size < 0) {
+            perror("read");
+            exit(1);
+        }
+    }
+    return size;
+}
+
+static EGLConfig get_config(struct render_state *s)
+{
+	EGLint egl_config_attribs[] = {
+		EGL_BUFFER_SIZE,	32,
+		EGL_DEPTH_SIZE,		EGL_DONT_CARE,
+		EGL_STENCIL_SIZE,	EGL_DONT_CARE,
+		EGL_RENDERABLE_TYPE,	EGL_OPENGL_ES2_BIT,
+		EGL_SURFACE_TYPE,	EGL_WINDOW_BIT,
+		EGL_NONE,
+	};
+
+	EGLint num_configs;
+	assert(eglGetConfigs(s->display, NULL, 0, &num_configs) == EGL_TRUE);
+
+	EGLConfig *configs = malloc(num_configs * sizeof(EGLConfig));
+	assert(eglChooseConfig(s->display, egl_config_attribs,
+			       configs, num_configs, &num_configs) == EGL_TRUE);
+	assert(num_configs);
+	printf("num config %d\n", num_configs);
+
+	// Find a config whose native visual ID is the desired GBM format.
+	for (int i = 0; i < num_configs; ++i) {
+		EGLint gbm_format;
+
+		assert(eglGetConfigAttrib(s->display, configs[i],
+					  EGL_NATIVE_VISUAL_ID, &gbm_format) == EGL_TRUE);
+		printf("gbm format %x\n", gbm_format);
+
+		if (gbm_format == GBM_FORMAT_ARGB8888) {
+			EGLConfig ret = configs[i];
+			free(configs);
+			return ret;
+		}
+	}
+
+	// Failed to find a config with matching GBM format.
+	abort();
+}
+
+void render_target_init(struct render_state *s)
+{
+	assert(epoxy_has_egl_extension(EGL_NO_DISPLAY, "EGL_MESA_platform_gbm"));
+
+	s->gbm = gbm_create_device(s->fd);
+	assert(s->gbm != NULL);
+
+	s->display = eglGetPlatformDisplayEXT(EGL_PLATFORM_GBM_MESA, s->gbm, NULL);
+	assert(s->display != EGL_NO_DISPLAY);
+
+	EGLint majorVersion;
+	EGLint minorVersion;
+	assert(eglInitialize(s->display, &majorVersion, &minorVersion) == EGL_TRUE);
+
+	assert(eglBindAPI(EGL_OPENGL_ES_API) == EGL_TRUE);
+
+	EGLConfig config = get_config(s);
+
+	s->gs = gbm_surface_create(
+		s->gbm, s->target_width, s->target_height, GBM_BO_FORMAT_ARGB8888,
+		GBM_BO_USE_LINEAR|GBM_BO_USE_SCANOUT|GBM_BO_USE_RENDERING);
+	assert(s->gs);
+
+	s->surface = eglCreatePlatformWindowSurfaceEXT(s->display, config, s->gs, NULL);
+	assert(s->surface != EGL_NO_SURFACE);
+
+	const EGLint contextAttribs[] = {
+		EGL_CONTEXT_CLIENT_VERSION, 2,
+		EGL_NONE
+	};
+	s->context = eglCreateContext(s->display, config, EGL_NO_CONTEXT, contextAttribs);
+	assert(s->context != EGL_NO_CONTEXT);
+
+	assert(eglMakeCurrent(s->display, s->surface, s->surface, s->context) == EGL_TRUE);
+}
+
+static GLuint compile_shader(const char *source, GLenum type)
+{
+	GLuint shader;
+	GLint compiled;
+
+	shader = glCreateShader(type);
+	glShaderSource(shader, 1, &source, NULL);
+	glCompileShader(shader);
+
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
+	if (!compiled) {
+		GLint infoLen = 0;
+		glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLen);
+		if (infoLen > 1) {
+			char *infoLog = malloc(infoLen);
+			glGetShaderInfoLog(shader, infoLen, NULL, infoLog);
+			fprintf(stderr, "Error compiling shader:\n%s\n", infoLog);
+			free(infoLog);
+		}
+		glDeleteShader(shader);
+		return 0;
+	}
+
+	return shader;
+}
+
+void init_gles(struct render_state *s, const char *vertex_shader,
+	       const char *fragment_shader)
+{
+	GLint linked;
+	GLuint vertexShader;
+	GLuint fragmentShader;
+	assert((vertexShader = compile_shader(vertex_shader, GL_VERTEX_SHADER)) != 0);
+	assert((fragmentShader = compile_shader(fragment_shader, GL_FRAGMENT_SHADER)) != 0);
+	assert((s->program = glCreateProgram()) != 0);
+	glAttachShader(s->program, vertexShader);
+	glAttachShader(s->program, fragmentShader);
+	glLinkProgram(s->program);
+	glGetProgramiv(s->program, GL_LINK_STATUS, &linked);
+	if (!linked) {
+		GLint infoLen = 0;
+		glGetProgramiv(s->program, GL_INFO_LOG_LENGTH, &infoLen);
+		if (infoLen > 1) {
+			char *infoLog = malloc(infoLen);
+			glGetProgramInfoLog(s->program, infoLen, NULL, infoLog);
+			fprintf(stderr, "Error linking program:\n%s\n", infoLog);
+			free(infoLog);
+		}
+		glDeleteProgram(s->program);
+		exit(1);
+	}
+
+	glViewport(0, 0, s->target_width, s->target_height);
+
+	glUseProgram(s->program);
+}
+
+void wait_fence(EGLDisplay display, int fd)
+{
+	EGLint attrib_list[] = {
+		EGL_SYNC_NATIVE_FENCE_FD_ANDROID, fd,
+		EGL_NONE
+	};
+	EGLSyncKHR sync =
+		eglCreateSyncKHR(display, EGL_SYNC_NATIVE_FENCE_ANDROID, attrib_list);
+	assert(sync != EGL_NO_SYNC_KHR);
+
+	assert(eglWaitSyncKHR(display, sync, 0) == EGL_TRUE);
+
+	eglDestroySyncKHR(display, sync);
+}
+
+int get_fence(EGLDisplay display)
+{
+	EGLSyncKHR sync =
+		eglCreateSyncKHR(display, EGL_SYNC_NATIVE_FENCE_ANDROID, NULL);
+	assert(sync != EGL_NO_SYNC_KHR);
+
+	/* generate sync fd */
+	glFlush();
+
+	int ret = eglDupNativeFenceFDANDROID(display, sync);
+	if (ret == EGL_NO_NATIVE_FENCE_FD_ANDROID)
+		ret = -1;
+
+	eglDestroySyncKHR(display, sync);
+	return ret;
+}
+
+int
+main(int argc, char **argv)
+{
+	int sv[2];
+	int pid;
+
+	if (socketpair(AF_LOCAL, SOCK_STREAM, 0, sv) < 0) {
+		perror("socketpair");
+		exit(1);
+	}
+
+	switch ((pid = fork())) {
+	case 0:
+		close(sv[0]);
+		client_main(sv[1]);
+		break;
+	case -1:
+		perror("fork");
+		exit(1);
+	default:
+		close(sv[1]);
+		server_main(sv[0]);
+		break;
+	}
+	return 0;
+}
